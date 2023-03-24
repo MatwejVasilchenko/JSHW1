f51b0862d4c37874202b8196eda94d507a9691d8
MatwejVasilchenko <110052092+MatwejVasilchenko@users.noreply.github.com>
2023-03-24 21:52:54 +0200

    AllWork JSHW1
    Nirvana

---
+++ JS/numbers.js
@@ -0,0 +1,21 @@
+const b = 75;
+const a = b / 2;
+console.log(a);
+
+const d = 123456789;
+const h = d - 10000;
+console.log(h);
+
+const f = -5;
+const e = f * -5;
+console.log(e);
+
+const s = 33;
+const g = s + 267;
+console.log(g);
+
+const c = 66;
+const x = 88;
+const v = 47;
+const z = x * x / v + c;
+console.log(z);
---
+++ JS/words.js
@@ -0,0 +1,29 @@
+let fruit = "apple";  
+console.log(fruit);
+const lengthFruit = fruit.length;
+console.log(lengthFruit);
+
+let vegetable = "wotherMelon";  
+console.log(vegetable);
+const lengthVegetable = vegetable.length;
+console.log(lengthVegetable);
+
+let berry = "cheery";  
+console.log(berry);
+const lengthBarry = berry.length;
+console.log(lengthBarry);
+
+let food = "all";  
+console.log(food);
+const lengthFood = food.length;
+console.log(lengthFood);
+
+let drink = "any";  
+console.log(drink);
+const lengthDrink = drink.length;
+console.log(lengthDrink);
+
+let fiend = "Artem";  
+console.log(fiend);
+const lengthFriendName = fiend.length;
+console.log(lengthFriendName);
